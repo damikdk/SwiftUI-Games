b6bf51280c8bc38e70494d9b31796ad19cb34d31
Damir Minnegalimov <damikdk@gmail.com>
2019-05-08 03:02:38 +0300

    change pressing handlers in GameVC

--- TBS/Game/GameVC.swift
+++ TBS/Game/GameVC.swift
@@ -9,10 +9,17 @@
 import UIKit
 import QuartzCore
 import SceneKit
+import SpriteKit
+
 
 enum BodyType: Int {
     case field = 1
     case material = 2
+}
+
+let defaultOnCharacterPress: ((GameVC, Character) -> Void) = { gameVC, charater in gameVC.currentCharacter = charater }
+let defaultOnFieldPress: ((GameVC, MaterialNode, Int, Int) -> Void) = { gameVC, cellNode, row, column in
+    gameVC.currentField.move(node: gameVC.currentCharacter!.node, toRow: row, column: column)
 }
 
 class GameVC: UIViewController, SCNPhysicsContactDelegate {
@@ -20,8 +27,11 @@ class GameVC: UIViewController, SCNPhysicsContactDelegate {
     var lightNode: SCNNode!
     var scene: SCNScene!
     var cameraHeight: Float = FieldConstants.defaultCellSize * 7
+ 
     var startScale: CGFloat = 0
     var lastScale: CGFloat = 1
+    var previousTranslateX:CGFloat = 0.0
+    var previousTranslateZ:CGFloat = 0.0
     
     var currentCharacter: Character?
     var characters: [Character] = []
@@ -49,6 +59,9 @@ class GameVC: UIViewController, SCNPhysicsContactDelegate {
             }
         }
     }
+    
+    var onCharacterPress: ((GameVC, Character) -> Void)!
+    var onFieldPress: ((GameVC, MaterialNode, Int, Int) -> Void)!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -65,11 +78,17 @@ class GameVC: UIViewController, SCNPhysicsContactDelegate {
 
         scene.rootNode.addChildNode(cameraNode)
         
+        // set the scene to the view
+        let scnView = self.view as! SCNView
+        scnView.scene = scene
+        scnView.backgroundColor = UIColor.black
+        
         // set up field and characters
         currentField = Field(in: SCNVector3())
-        
         createCharacters(random: false)
-        
+        onCharacterPress = defaultOnCharacterPress
+        onFieldPress = defaultOnFieldPress
+
         // create and add a light to the scene
         let light = SCNLight()
         lightNode = SCNNode()
@@ -89,11 +108,6 @@ class GameVC: UIViewController, SCNPhysicsContactDelegate {
         ambientLightNode.light!.type = .ambient
         ambientLightNode.light!.color = UIColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
-        
-        // set the scene to the view
-        let scnView = self.view as! SCNView
-        scnView.scene = scene
-        scnView.backgroundColor = UIColor.black
         
         // allows the user to manipulate the camera
         // scnView.allowsCameraControl = true
@@ -161,6 +175,18 @@ class GameVC: UIViewController, SCNPhysicsContactDelegate {
     func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact) {
     }
     
+    func findCharacter(by gameID: String!) -> Character? {
+        for character in characters {
+            if character.gameID == gameID {
+                return character
+            }
+        }
+        
+        return nil
+    }
+}
+
+extension GameVC {
     @objc
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
         // retrieve the SCNView
@@ -194,10 +220,10 @@ class GameVC: UIViewController, SCNPhysicsContactDelegate {
                 .components(separatedBy: FieldConstants.indexSeparator)
                 .map { Int($0)!}
             
-            currentField.move(node: currentCharacter!.node, toRow: fieldIndexes[0], column: fieldIndexes[1])
+            onFieldPress(self, materialNode!, fieldIndexes[0], fieldIndexes[1])
         } else if (materialNode!.type == .material) {
             if let tappedCharacter = findCharacter(by: materialNode!.gameID!) {
-                currentCharacter = tappedCharacter
+                onCharacterPress(self, tappedCharacter)
             }
         }
     }
@@ -207,31 +233,28 @@ class GameVC: UIViewController, SCNPhysicsContactDelegate {
         if sender.numberOfTouches == 2 {
             let zoom = sender.scale
             let fov = 1 / (startScale * zoom)
-
+            
             if (sender.state == .began) {
                 startScale = lastScale
             } else if (sender.state == .changed && fov < 95) {
-
+                
                 cameraNode.camera?.fieldOfView = CGFloat(fov)
                 lastScale = startScale * zoom
             }
         }
     }
-
-    var previousTranslateX:CGFloat = 0.0
-    var previousTranslateZ:CGFloat = 0.0
-
+    
     @objc
     func handlePan(_ sender: UIPanGestureRecognizer) {
         let currentTranslateX = sender.translation(in: view!).x
         let currentTranslateZ = sender.translation(in: view!).y
         
         print(currentTranslateX, currentTranslateZ)
-
+        
         // calculate translation since last measurement
         let translateX = (currentTranslateX - previousTranslateX) / 25
         let translateZ = (currentTranslateZ - previousTranslateZ) / 25
-
+        
         let oldPosition = cameraNode.position
         
         let newCamPosition = SCNVector3(oldPosition.x - Float(translateX), oldPosition.y, oldPosition.z - Float(translateZ))
@@ -246,18 +269,7 @@ class GameVC: UIViewController, SCNPhysicsContactDelegate {
             previousTranslateZ = currentTranslateZ
         }
     }
-    
-    func findCharacter(by gameID: String!) -> Character? {
-        for character in characters {
-            if character.gameID == gameID {
-                return character
-            }
-        }
-        
-        return nil
-    }
 }
-
 
 extension GameVC {
     override var shouldAutorotate: Bool {
