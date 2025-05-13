4c1e90cd4edda9caa866376b8ab88f0765820e4f
Damir Minnegalimov <damikdk@gmail.com>
2025-05-13 15:15:19 +0300

    Recreate project in Xcode 16.3

---
+++ SwiftUI-Games/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  SwiftUI-Games
+//
+//  Created by damikdk on 13.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MenuView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Games/SwiftUI_GamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_GamesApp.swift
+//  SwiftUI-Games
+//
+//  Created by damikdk on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_GamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
